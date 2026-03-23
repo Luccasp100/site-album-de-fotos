c4ccf9bb50aa9c3b70507ccc8b05593325f393ca
Luccas Fernandes <luccas.pprofissional@gmail.com>
2026-03-23 14:20:45 -0300

    Adicionando botão e dark-mode

--- src/Pages/NavBar/NavBar.jsx
+++ src/Pages/NavBar/NavBar.jsx
@@ -1,8 +1,24 @@
+import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import './NavBar.css';
 
 function Navbar() {
     const location = useLocation();
+
+    //armazena o estado atual do dark-mode
+    const [isDarkMode, setIsDarkMode] = useState(() => {
+        return localStorage.getItem('theme') === 'dark';
+    });
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light');
+        }
+    }, [isDarkMode]);
 
     return (
         <nav className='home-container-nav'>
@@ -10,6 +26,19 @@ function Navbar() {
             <Link to="/imagemX" className={`home-container-nav-button ${location.pathname === '/imagemX' ? 'active-link' : ''}`}>imagemX</Link>
             <Link to="/imagemY" className={`home-container-nav-button ${location.pathname === '/imagemY' ? 'active-link' : ''}`}>imagemY</Link>
             <Link to="/imagemZ" className={`home-container-nav-button ${location.pathname === '/imagemZ' ? 'active-link' : ''}`}>imagemZ</Link>
+
+            <div className="dark-mode-container">
+                <label className="switch">
+                    <input
+                        type="checkbox"
+                        checked={isDarkMode}
+                        onChange={() => setIsDarkMode(!isDarkMode)}
+                    />
+                    <span className="slider">
+                        <span className="icon">{isDarkMode ? '🌙' : '☀️'}</span>
+                    </span>
+                </label>
+            </div>
         </nav>
     );
 }
